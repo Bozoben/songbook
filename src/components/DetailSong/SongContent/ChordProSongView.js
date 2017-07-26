85e512bb8987097da1a4c1a15508d6510f559ea8
bvi <Benoit.VILLETARD@maif.fr>
2017-07-26 15:32:18 +0200

    Ebauche visu chordpro

--- src/components/DetailSong/SongContent/ChordProSongView.js
+++ src/components/DetailSong/SongContent/ChordProSongView.js
@@ -1,24 +1,42 @@
+const e = React.createElement;
 
 function superLine(item) {
+  // on decompose la ligne en n tokens (soit du texte, soit un accord)
+  var tokens = [];
+  var newItem = item;
   var regExp = /\[([^\]]+)\]/g ;
   var accords = item.match(regExp);
-  if (accords != null)
-  accords.forEach(function(machin) {
-    console.log("Accord",machin);
-  });  
-  const bidule=<div>{item}</div>;
-  return (<p>{bidule}</p>)
+  if (accords == null) {
+    tokens.push(item);
+  } else {
+    accords.forEach(function(accord) {
+      var idx = newItem.indexOf(accord);
+      const accordBrut = accord.replace('[','').replace(']','');
+      if (idx > 0)
+        tokens.push(e("div",{className:"textToken"},newItem.substr(0,idx)));
+      tokens.push(e("div",{className:"chord"}, accordBrut));
+      newItem = newItem.substr(idx + accord.length);
+      console.log("New item",newItem);
+    });
+    if (newItem.length > 0)
+      tokens.push(e("div",{className:"textToken"},newItem));
+  }
+
+  //var allTokens = e("div",null,tokens);
+  return (e("div",{className:"lineWithChords"},tokens));
 }
+
+import "./ChordProSongView.css";
 
 class ChordProSongView extends React.Component {
   render() {
     return(
-      <pre>
+      <div><br/>
       {this.props.content.map(function(item,idx){
       return (
         superLine(item)
       )})}
-      </pre>
+      </div>
     )
   }
 }
